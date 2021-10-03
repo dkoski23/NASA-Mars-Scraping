b13c2699bce327d602d8923f1a74b64d915f7c4b
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-10-02 21:33:44 -0700

    Add files via upload
    I just successfully tested all functions via Flask run in the terminal.

--- template/mission_to_mars.py
+++ template/mission_to_mars.py
@@ -12,17 +12,28 @@ import pymongo
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
 
-#setup browser variable for Chrome, making sure splinter is setup and 
-#updated
 def scrape():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
+    
     mars_latest_news(browser)
     news_title, news_p = mars_latest_news(browser)
     
+    featured_image(browser)
+    full_image_url = featured_image(browser)
+    
+    mars_facts()
+    html_mars_facts = mars_facts()
+    
+    hemispheres(browser)
+    hemisphere_image_urls =  hemispheres(browser)
+    
     data = {
-        "news_title" : news_title,
-        "news_paragraph" : news_p
+        "Latest News Title" : news_title,
+        "News Paragraph" : news_p,
+        "Featured Image URL" : full_image_url,
+        "Mars Facts html" : html_mars_facts,
+        "Hemisphere URLS" : hemisphere_image_urls
     }
     print(data)
     browser.quit()
@@ -39,118 +50,86 @@ def mars_latest_news(browser):
     
     content_titles = soup.find_all('div', class_='content_title')
     further = content_titles[1]
-    further
     news_title = further.text
-    news_title
 
     news_p = soup.find('div', class_='article_teaser_body').text
     
     return news_title, news_p
 
 
-# ## Featured Image Scraping
+## Featured Image Scraping
 
-# def featured_image(browser):
-#     image_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
-
-#     browser.visit(image_url)
-#     html = browser.html
-#     soup = bs(html, 'html.parser')
-#     image_full = soup.find('img', class_="headerimage fade-in")['src']
-#     image_full
-
-
-
-#     full_image_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/" + image_full
-
-
-
-#     print(full_image_url)
-#     return full_image_url
-
-
-# # ## On to Mars Facts
-
-# import os, ssl
-# if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-# getattr(ssl, '_create_unverified_context', None)):
-#     ssl._create_default_https_context = ssl._create_unverified_context
-
-
-
-# mars_facts_df = pd.read_html('https://space-facts.com/mars/')
-
-
-# #this is not a dataframe at the moment, it is a big list, but the
-# #first item has the values desired for this project
-# print(mars_facts_df)
-
-
-# mars_facts_df = mars_facts_df[0]
-
-
-# print(mars_facts_df)
-
-
-# #name the columns
-# mars_facts_df.columns=['Statistic', "Value"]
-
-# (mars_facts_df)
-
-
-# html_mars_facts = mars_facts_df.to_html()
-# print(html_mars_facts)
-
-
-# print(type(html_mars_facts))
-
-
-# # ## Onto Hemispheres
-# def hemispheres(browser):
+def featured_image(browser):
+    image_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
+    browser.visit(image_url)
+    html = browser.html
+    soup = bs(html, 'html.parser')
     
-#     hemis_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-#     browser.visit(hemis_url)
+    image_full = soup.find('img', class_="headerimage fade-in")['src']
 
-#     html = browser.html
-#     soup = bs(html, 'html.parser')
-#     links_from_home = []
+    full_image_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/" + image_full
+
+    print(full_image_url)
+    return full_image_url
+
+
+# ## On to Mars Facts
+def mars_facts():
+    import os, ssl
+    if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)):
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+    mars_facts_df = pd.read_html('https://space-facts.com/mars/')
+
+    mars_facts_df = mars_facts_df[0]
+
+    mars_facts_df.columns=['Statistic', "Value"]
+    html_mars_facts = mars_facts_df.to_html()
     
-#     list_h = soup.find_all('a', class_='itemLink product-item')
-#     only_one = []
+    return html_mars_facts
 
 
-#     for link in list_h:
-#         #print(link)
-#         #print('--------------------')
-#         if link['href'] not in only_one:
-#             #print(link['href'])
-#             only_one.append(link['href'])
 
-#     print(only_one)
+# ## Onto Hemispheres
+def hemispheres(browser):
+    
+    hemis_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(hemis_url)
 
-#     part_one = 'https://astrogeology.usgs.gov'
-#     for link in only_one:
-#         links_from_home.append(part_one + link)
-#     links_from_home
-#     hemisphere_image_urls = []
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    links_from_home = []
+    
+    list_h = soup.find_all('a', class_='itemLink product-item')
+    only_one = []
 
-#     for link in link_from_home:
-#         hemisphere_dict = {}
-#         browser.visit(link)
-#         html = browser.html
-#         soup = bs(html, 'html.parser')
-#         test = browser.find_by_css('h2.title').text
-#         print(test)
-#         hemisphere_dict['title'] = test
-#         image_url = soup.find('img', class_='wide-image')['src']
-#         image_url = part_one + image_url
-#         print(image_url)
-#         hemisphere_dict['img_url'] = image_url
-#         hemisphere_image_urls.append(hemisphere_dict)
+    for link in list_h:
+        #print(link)
+        #print('--------------------')
+        if link['href'] not in only_one:
+            #print(link['href'])
+            only_one.append(link['href'])
 
+    part_one = 'https://astrogeology.usgs.gov'
+    for link in only_one:
+        links_from_home.append(part_one + link)
+    links_from_home
+    hemisphere_image_urls = []
 
-#     print(hemisphere_image_urls)
-#     return hemisphere_image_urls
+    for link in links_from_home:
+        hemisphere_dict = {}
+        browser.visit(link)
+        html = browser.html
+        soup = bs(html, 'html.parser')
+        test = browser.find_by_css('h2.title').text
+        hemisphere_dict['title'] = test
+        image_url = soup.find('img', class_='wide-image')['src']
+        image_url = part_one + image_url
+        hemisphere_dict['img_url'] = image_url
+        hemisphere_image_urls.append(hemisphere_dict)
+
+    return hemisphere_image_urls
 
 
 
